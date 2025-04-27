3c55397a84d5958b37e4e0156137ff196519f3bc
peter-mwaura <peterkaranjagitz@gmail.com>
2025-04-27 21:21:24 +0300

    feat(dashboard): implement logic for displaying users in dashboard

--- src/components/dashboard/search/search.tsx
+++ src/components/dashboard/search/search.tsx
@@ -3,16 +3,22 @@ import { MdSearch } from 'react-icons/md';
 
 interface SearchProps {
     placeholder: string;
+    onSearch: (searchTerm: string) => void; // Adding the onSearch prop
 }
 
-const Search = ({ placeholder }: SearchProps) => {
+const Search = ({ placeholder, onSearch }: SearchProps) => {
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        onSearch(event.target.value); // Trigger the onSearch function with the input value
+    };
+
     return (
         <div className="flex items-center gap-2.5 bg-[#2e374a] p-2.5 rounded-[10px] w-max">
-            <MdSearch className="" />
+            <MdSearch className="text-white" />
             <Input
                 className="bg-transparent border-none text-white outline-none"
                 type="text"
                 placeholder={placeholder}
+                onChange={handleSearchChange} // Attach onChange handler
             />
         </div>
     );
